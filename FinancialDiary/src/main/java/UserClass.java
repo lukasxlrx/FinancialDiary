b77242528f838f8dc71000afd5fe360463e22d52
DennisAummarin <2005308H@student.tp.edu.sg>
2022-01-25 16:09:56 +0800

    Done userclass and loginServlet

--- FinancialDiary/src/main/java/UserClass.java
+++ FinancialDiary/src/main/java/UserClass.java
@@ -1,4 +1,55 @@
 
 public class UserClass {
+	protected int UserID;
+	protected String username;
+	protected String password;
+	public UserClass(int userID, String username, String password, String email, String firstname, String lastname) {
+		super();
+		UserID = userID;
+		this.username = username;
+		this.password = password;
+		this.email = email;
+		this.firstname = firstname;
+		this.lastname = lastname;
+	}
+	public int getUserID() {
+		return UserID;
+	}
+	public void setUserID(int userID) {
+		UserID = userID;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getFirstname() {
+		return firstname;
+	}
+	public void setFirstname(String firstname) {
+		this.firstname = firstname;
+	}
+	public String getLastname() {
+		return lastname;
+	}
+	public void setLastname(String lastname) {
+		this.lastname = lastname;
+	}
+	protected String email;
+	protected String firstname;
+	protected String lastname;
 
 }
