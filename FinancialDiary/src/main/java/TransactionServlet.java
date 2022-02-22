71085fde3107895b51f48d6c38c927138b20284c
Peng Yong Shun <pys301199@gmail.com>
2022-02-22 22:53:24 +0800

    testing

--- FinancialDiary/src/main/java/TransactionServlet.java
+++ FinancialDiary/src/main/java/TransactionServlet.java
@@ -321,7 +321,7 @@ public class TransactionServlet extends HttpServlet {
 		}
 		return true;
 	}
-	//testing again and again and again
+	//testing 
 	public boolean DeleteTransaction(int TransactionID) 
 		{
 		try (Connection connection = getConnection();
