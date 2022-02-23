fd68aab19cc3ab2e388954230a0e80c45ca0987e
leerongxuanlukas <2002517G@student.tp.edu.sg>
2022-02-23 16:32:17 +0800

    added back registercollection

---
+++ FinancialDiary/src/test/java/FinancialDiary/RegisterCollectionTest.java
@@ -0,0 +1,25 @@
+package FinancialDiary;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class RegisterCollectionTest {
+	private RegisterCollection rc;
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+	}
+
+	@Test
+	void testAddUser() {
+		fail("Not yet implemented");
+	}
+
+}
